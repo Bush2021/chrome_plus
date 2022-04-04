75567ea5fbba467d64f6703ae03a134393130598
BushYan <79072750+Bush2021@users.noreply.github.com>
2022-04-04 13:19:32 +0800

    Add files via upload

--- src/portable.h
+++ src/portable.h
@@ -105,10 +105,12 @@ std::wstring GetCommand(LPWSTR param)
         // ×·¼Ó²ÎÊý
         if (i == insert_pos)
         {
-            args.push_back(L"--shuax");
+            args.push_back(L"--bush");
 
 			// args.push_back(L"--force-local-ntp");
             args.push_back(L"--disable-background-networking");
+            
+            args.push_back(L"--disable-crash-reporter");
 
 			args.push_back(L"--disable-features=RendererCodeIntegrity,FlashDeprecationWarning");
 
