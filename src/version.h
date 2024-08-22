d699e08234ca25369cc9c99328a994d165a3caad
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2024-08-22 11:45:14 -0400

    build(release): bump version to 1.9.0

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H_
 
 #define RELEASE_VER_MAIN 1
-#define RELEASE_VER_SUB 8
-#define RELEASE_VER_FIX 9
+#define RELEASE_VER_SUB 9
+#define RELEASE_VER_FIX 0
 
 #define TOSTRING2(arg) #arg
 #define TOSTRING(arg) TOSTRING2(arg)
