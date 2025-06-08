b3bb52611802bc6c62ccf7d1435827bf2abf5a33
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2025-06-08 14:10:00 -0400

    build(release): bump version to 1.12.0 (#159)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H_
 
 #define RELEASE_VER_MAIN 1
-#define RELEASE_VER_SUB 11
-#define RELEASE_VER_FIX 1
+#define RELEASE_VER_SUB 12
+#define RELEASE_VER_FIX 0
 
 #define TOSTRING2(arg) #arg
 #define TOSTRING(arg) TOSTRING2(arg)
