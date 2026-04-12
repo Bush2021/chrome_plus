ca7ccaa5ba15a85385c76a4047addc6177172865
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2026-04-12 19:58:16 -0400

    ci: support semantic versioning with pre-release suffix

--- src/version.h
+++ src/version.h
@@ -4,12 +4,15 @@
 #define RELEASE_VER_MAIN 1
 #define RELEASE_VER_SUB 15
 #define RELEASE_VER_FIX 2
+#define RELEASE_VER_PRE_SUFFIX ""  // e.g. "-alpha.1", "-rc.2", or "" for stable
 
 #define TOSTRING2(arg) #arg
 #define TOSTRING(arg) TOSTRING2(arg)
 
-#define RELEASE_VER_STR      \
-  TOSTRING(RELEASE_VER_MAIN) \
-  "." TOSTRING(RELEASE_VER_SUB) "." TOSTRING(RELEASE_VER_FIX)
+#define RELEASE_VER_STR                         \
+  TOSTRING(RELEASE_VER_MAIN)                    \
+  "." TOSTRING(RELEASE_VER_SUB)                 \
+  "." TOSTRING(RELEASE_VER_FIX)                 \
+  RELEASE_VER_PRE_SUFFIX
 
 #endif  // CHROME_PLUS_SRC_VERSION_H_
